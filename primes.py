5fd96b2ec1d34cfd4de75679d02036dde3faee80
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-13 02:27:37 +0100

    Check solution

--- primes.py
+++ primes.py
@@ -2,24 +2,23 @@ from math import sqrt
 
 """List of prime numbers generator."""
 
-try:
-    def primes(number_of_primes):
-        list = []
-        temp = 0
-        while (number_of_primes != 0):
-            check = True
-            if (temp>1):
-                #check from 2 to the square root of temp - prime in only those circumstances
-                for i in range(2, int(sqrt(temp))+1):
-                    if (temp % i == 0):
-                        check = False
-                #if temp is prime, add it to list
-                if (check == True):
-                    list.append(temp)
-                    number_of_primes = number_of_primes - 1
-            check=False
-            temp = temp + 1
+def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError("ValueError exception thrown")
+    list = []
+    temp = 0
+    while (number_of_primes != 0):
+        check = True
+        if (temp>1):
+            #check from 2 to the square root of temp - prime in only those circumstances
+            for i in range(2, int(sqrt(temp))+1):
+                if (temp % i == 0):
+                    check = False
+            #if temp is prime, add it to list
+            if (check == True):
+                list.append(temp)
+                number_of_primes = number_of_primes - 1
+        check=False
+        temp = temp + 1
 
-        return list
-except ValueError:
-    raise ValueError("ValueError exception thrown")
+    return list
